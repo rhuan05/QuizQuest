4d0f38fe0e69ec992d52c50c985281c91f1363fd
rhuan05 <rhuanhenrique964@gmail.com>
2025-07-29 20:26:59 -0300

    Criando a base da API de acordo com a variável de ambiente | VITE_BASE_URL_API

--- client/src/lib/queryClient.ts
+++ client/src/lib/queryClient.ts
@@ -1,5 +1,7 @@
 import { QueryClient, QueryFunction } from "@tanstack/react-query";
 
+const BASE_URL_API = import.meta.env.VITE_BASE_URL_API;
+
 async function throwIfResNotOk(res: Response) {
   if (!res.ok) {
     const text = (await res.text()) || res.statusText;
@@ -12,7 +14,7 @@ export async function apiRequest(
   url: string,
   data?: unknown | undefined,
 ): Promise<Response> {
-  const res = await fetch(url, {
+  const res = await fetch(`${BASE_URL_API}${url}`, {
     method,
     headers: data ? { "Content-Type": "application/json" } : {},
     body: data ? JSON.stringify(data) : undefined,
