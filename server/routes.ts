1184102a189d1614678b58a47abaa25b3e261ba9
rhuan05 <rhuanhenrique964@gmail.com>
2025-08-01 16:58:04 -0300

    Removendo validação para erro em caso de zeo Quizzes completos

--- server/routes.ts
+++ server/routes.ts
@@ -257,9 +257,6 @@ export async function registerRoutes(app: Express): Promise<Server> {
   app.get("/api/quiz/dashboard", async (req, res) => {
     try {
       const completedQuizzes = await storage.getCompletedQuizSession();
-      if (!completedQuizzes || completedQuizzes.length === 0) {
-        return res.status(404).json({ message: "No completed quizzes found" });
-      }
 
       res.json(completedQuizzes);
     } catch (error) {
